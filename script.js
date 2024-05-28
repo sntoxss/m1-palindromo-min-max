d981c86d3c9b68aadfde6d271a4355be14324884
Sntoxss <contato.sntos@gmail.com>
2024-05-28 11:12:24 -0300

    mudança no código javaScript e no title do html
    Criei a resolução do código pedido, no projeto 1 e no projeto 2

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 function isPalindrome(str){
-    /* Seu código aqui */
-}
+    let invertida = "";
+    str = str.toLowerCase().replace(/\s/g, '')
+    
 
+    for(let i = str.length -1; i >= 0 ; i-- ){
+        invertida += str[i];
+    }
+
+    if(invertida ==  str && typeof invertida == "string"){
+        return true;
+    }
+    return false;
+}
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let maiorNumeroEncontrado = arr[0];
+    let menorNumeroEncontrado = arr[0];
+
+    for(let i = 1 ; i < arr.length; i++){
+        if(arr[i] > maiorNumeroEncontrado){
+            maiorNumeroEncontrado = arr[i];
+        }
+        if(arr[i] < menorNumeroEncontrado){
+            menorNumeroEncontrado = arr[i];
+        }
+    }
+    let arrTodosOsValoresMenorMaior = [menorNumeroEncontrado, maiorNumeroEncontrado]
+    return arrTodosOsValoresMenorMaior
 }
